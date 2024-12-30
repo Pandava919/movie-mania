8394fcd9f410737a5fcaa99f0ca872b2e1b6edfb
Pandava919 <pandavapandu919@gmail.com>
2024-12-30 11:21:02 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Omdb from './components/Omdb'
+import MovieDetails from './components/MovieDetails'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Omdb />
         <Routes>
-          <Route />
+          <Route path='/' element={<Omdb />} />
+          <Route path='/movies' element={<MovieDetails />} />
         </Routes>
       </BrowserRouter>
     </div>
