2ae029d2849d8e6660b21835a7dfd0a6a127ff51
Pandava919 <pandavapandu919@gmail.com>
2025-01-05 23:04:16 +0530

    movie not found image added

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import notFound from '../assets/no_image.jpg';
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 // import Slider from 'react-slick'
@@ -7,7 +8,7 @@ const Movie = ({ poster, title, year }) => {
 
     return (
         <div className=' h-60 sm:h-60 hover:-translate-y-2 duration-300 w-60 flex flex-col items-center rounded-lg bg-red-700 '>
-            <img src={poster !== 'N/A' ? poster : "https://imgs.search.brave.com/QrrF8yctvnxGKn5UBvuEt1XL7Pv04zXmzQ0y50RN5cY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA3LzkxLzIyLzU5/LzM2MF9GXzc5MTIy/NTkyN19jYVJQUEg5/OUQ2RDFpRm9ua0NS/bUNHemtKUGYzNlFE/dy5qcGc"} className='h-40 w-48 m-2 rounded-md' />
+            <img src={poster !== 'N/A' ? poster : notFound} className='h-40 w-48 m-2 rounded-md' />
             <h2 className='w-48 text-ellipsis flex items-center justify-center'> <span className='text-white truncate text-sm font-light'>{title}</span></h2>
             <h4 className='text-white font-light'>{year}</h4>
         </div>
