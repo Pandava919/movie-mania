88e9e0fc2b7f9157ca579fa9a8d5d693b4779be4
Pandava919 <pandavapandu919@gmail.com>
2025-01-05 21:59:51 +0530

    style removed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ onClickHandler, enter }) => {
     return (
-        <div onClick={onClickHandler} className='cursor-pointer rounded-md p-1.5 px-3 flex items-center justify-center active:bg-red-900 bg-red-800 w-1/4 sm:w-20' >
+        <div onClick={onClickHandler} className='cursor-pointer' >
             <span className='text-white text-center text-sm sm:text-lg'>{enter}</span>
         </div>
     )
