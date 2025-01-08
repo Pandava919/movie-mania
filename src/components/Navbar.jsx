4016375d57d5345dbe5a1e9696d0a309b6d5bbac
Pandava919 <pandavapandu919@gmail.com>
2025-01-08 10:18:08 +0530

    navbar component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='bg-red-800 h-14 flex items-center justify-start w-full' >
+                <div className='ml-3 h-14 w-1/4 flex items-center justify-start'>
+                    <h1 className='text-white text-sm font-light sm:text-2xl md:text-4xl '>Movie Mania</h1>
+                </div>
+            </nav>
+        </>
+
+
+    )
+}
+
+export default Navbar
