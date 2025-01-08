62f754720879b24bd38845647eabe7d13e81a699
Pandava919 <pandavapandu919@gmail.com>
2025-01-08 10:19:09 +0530

    fetched the movie data with id

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch';
 
 const MovieDetails = () => {
+    const { id } = useParams()
+    console.log(id);
+    const apiurl = import.meta.env.VITE_OMDB_API_URL;
+    const apikey = import.meta.env.VITE_OMDB_API_KEY;
+    const { apiData, loading } = useFetch(`${apiurl}?apikey=${apikey}&i=${id}`)
+    console.log(apiData);
+
     return (
         <div>
-            Movies
+
         </div>
     )
 }
