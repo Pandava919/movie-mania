430406550f7ad9d2aa14ef291bbbc6c2184be120
Pandava919 <pandavapandu919@gmail.com>
2025-01-14 15:09:50 +0530

    Fetched movie using id

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch';
 import Navbar from './Navbar';
 
 const MovieDetails = () => {
     const { id } = useParams()
-    console.log(id);
+    const [movie,setMovie] = useState();
     const apiurl = import.meta.env.VITE_OMDB_API_URL;
     const apikey = import.meta.env.VITE_OMDB_API_KEY;
     const { apiData, loading } = useFetch(`${apiurl}?apikey=${apikey}&i=${id}`, id);
+    useEffect(()=>{
+        setMovie(apiData);
+    },[apiData])
+    console.log(movie);
+    
+    
 
     return (
         <section className='min-h-screen w-screen bg-black'>
             <div>
                 <Navbar logo='MovieMania' />
             </div>
-            <section>
+            <section className='w-screen h-full bg-black'>
+                <div>
+                    <div>
+                        
+                    </div>
+                </div>
+                <div>
 
+                </div>
             </section>
         </section>
     )
