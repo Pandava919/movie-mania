e985cef9bb3608ccc8f50352189a46c2210c6c96
Pandava919 <pandavapandu919@gmail.com>
2025-01-15 19:12:35 +0530

    position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = ({logo}) => {
     return (
         <>
-            <nav className='bg-red-800 h-14 flex items-center justify-start w-full' >
+            <nav className='bg-red-800 h-14 flex items-center justify-start w-full fixed' >
                 <div className='ml-3 h-14 w-1/4 flex items-center justify-start'>
                     <h1 className='text-white text-sm font-light sm:text-2xl md:text-4xl '>{logo}</h1>
                 </div>
